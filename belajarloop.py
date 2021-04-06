a9b2edd8000c9762a04b87b3a45e99dad7824e5d
Agung Adiwidjaya <houren_soda@yahoo.com>
2021-04-06 11:53:28 +0700

    nge test list comprehensi sekalian

--- belajarloop.py
+++ belajarloop.py
@@ -1,6 +1,9 @@
 print("Test loop")
-a = "loop" #ini untuk string yang mao di looping
+a = range(0, 10) #ini untuk string yang mao di looping
 for _ in a:
   print(_)
+  
+b = [f**2 for f in a]
+
   
   
